0d012a6a85ff34f2992a3d936fb9d1f07f85f0c0
pierre golfier <pgolfier.pro@gmail.com>
2022-11-18 21:18:21 +0100

    feat(post): impl turbo frame tag for posts
    also add a placeholder and update post partial

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,21 @@
 #
 class Post < ApplicationRecord
   validates :author, :body, presence: true
+
+  after_create_commit { broadcast_prepend_to "posts" }
+  after_update_commit { broadcast_replace_to "posts" }
+  after_destroy_commit { broadcast_remove_to "posts" }
+
+  def relative_created_at
+    created_at_from_now = Time.current - created_at
+    if created_at_from_now < 1.minute
+      "#{(created_at_from_now / 1.second).to_i}sec."
+    elsif created_at_from_now < 1.hour
+      "#{(created_at_from_now / 1.minute).to_i}min."
+    elsif created_at_from_now < 1.day
+      "#{(created_at_from_now / 1.hour).to_i}h."
+    else
+      "#{(created_at_from_now / 1.day).to_i}j."
+    end
+  end
 end
