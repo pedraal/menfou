f280d9096bc9beb49d80aeb0f5ef16d0a309b708
pierre golfier <pgolfier.pro@gmail.com>
2022-11-20 01:38:54 +0100

    test: comment out not maintained system tests

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -1,41 +1,42 @@
 require "application_system_test_case"
 
 class PostsTest < ApplicationSystemTestCase
-  setup do
-    @post = posts(:one)
-  end
+  # Scaffolded and not maintained yet
+  # setup do
+  #   @post = posts(:one)
+  # end
 
-  test "visiting the index" do
-    visit posts_url
-    assert_selector "h1", text: "Posts"
-  end
+  # test "visiting the index" do
+  #   visit posts_url
+  #   assert_selector "h1", text: "Posts"
+  # end
 
-  test "should create post" do
-    visit posts_url
-    click_on "New post"
+  # test "should create post" do
+  #   visit posts_url
+  #   click_on "New post"
 
-    fill_in "body", with: @post.body
-    click_on "Create Post"
+  #   fill_in "body", with: @post.body
+  #   click_on "Create Post"
 
-    assert_text "Post was successfully created"
-    click_on "Back"
-  end
+  #   assert_text "Post was successfully created"
+  #   click_on "Back"
+  # end
 
-  test "should update Post" do
-    visit post_url(@post)
-    click_on "Edit this post", match: :first
+  # test "should update Post" do
+  #   visit post_url(@post)
+  #   click_on "Edit this post", match: :first
 
-    fill_in "body", with: @post.body
-    click_on "Update Post"
+  #   fill_in "body", with: @post.body
+  #   click_on "Update Post"
 
-    assert_text "Post was successfully updated"
-    click_on "Back"
-  end
+  #   assert_text "Post was successfully updated"
+  #   click_on "Back"
+  # end
 
-  test "should destroy Post" do
-    visit post_url(@post)
-    click_on "Destroy this post", match: :first
+  # test "should destroy Post" do
+  #   visit post_url(@post)
+  #   click_on "Destroy this post", match: :first
 
-    assert_text "Post was successfully destroyed"
-  end
+  #   assert_text "Post was successfully destroyed"
+  # end
 end
