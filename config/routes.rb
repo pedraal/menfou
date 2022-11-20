ad4675e5529a98d9a417d63660f1de1335259d7d
pierre golfier <pgolfier.pro@gmail.com>
2022-11-20 22:39:54 +0100

    chore: cleanup unused code

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,4 @@ Rails.application.routes.draw do
   get '/auth/auth0/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
   get '/auth/logout' => 'auth0#logout'
-
-  get '/dashboard' => 'dashboard#show'
 end
