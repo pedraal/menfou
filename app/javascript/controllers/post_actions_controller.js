04ed45a69ede6e6fcd7bb1bcb529058283649ca8
pierre golfier <pgolfier.pro@gmail.com>
2022-11-20 22:38:42 +0100

    feat(post): add user link

--- app/javascript/controllers/post_actions_controller.js
+++ app/javascript/controllers/post_actions_controller.js
@@ -2,10 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 import { get, destroy } from "@rails/request.js"
 
 export default class extends Controller {
-  static values = { editUrl: String, showUrl: String }
+  static values = { showUrl: String, showUserUrl: String, editUrl: String }
 
   show() {
     window.location = this.showUrlValue
+  }
+
+  showUser() {
+    window.location = this.showUserUrlValue
   }
 
   edit() {
