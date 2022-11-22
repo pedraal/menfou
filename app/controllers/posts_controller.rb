ff2ea6be9ed335f954dcd9a837e3c72b8b075ece
pierre golfier <pgolfier.pro@gmail.com>
2022-11-22 07:49:07 +0100

    fix(posts): top followers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.where(user: current_user.followee_users).or(Post.where(user: current_user)).order(id: :desc).page(params[:page])
     @top_posters = User.order(posts_count: :desc).limit(3)
-    @top_followees = User.order(followees_count: :desc).limit(3)
+    @top_followers = User.order(followers_count: :desc).limit(3)
   end
 
   # GET /posts/1
